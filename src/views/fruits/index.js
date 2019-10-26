7923d67d86086886545d0bd987dd8762cdcf94de
keke0123 <keke0123@naver.com>
2019-10-26 14:48:46 +0900

    이전 커밋에 잡은 방법 틀림

--- src/views/fruits/index.js
+++ src/views/fruits/index.js
@@ -6,19 +6,30 @@ import {storeContext} from '../../store/context';
 function Fruits() {
     const [test, setTest] = useState();
     const store = React.useContext(storeContext);
-    console.log('storeContext', storeContext);
-    console.log('store', store);
-    const fruitStore = store.fruitStore();
-    console.log('allFruits', fruitStore.allFruits);
-    fruitStore.setQuery('hihi');
-    console.log('query', fruitStore.query.get());
+    // console.log('storeContext', storeContext);
+    // console.log('store', store);
+
+    // const fruitStore = store.fruitStore();
+
+    // console.log('allFruits', fruitStore.allFruits);
+    // fruitStore.setQuery('hihi');
+    // console.log('query', fruitStore.query.get());
 
     // console.log('test', useStoreData(storeContext, contextData=>contextData, store=>store.allFruits));
+    function changeValue() {
+        console.log('change value');
+        // fruitStore.setQuery('test success');
+        store.setQuery('test success');
+    }
     
     return useObserver(() => {
         return (
             <div>
                 fruits page
+                <div>
+                    {store.query.get()}
+                </div>
+                <button onClick={changeValue}>test</button>
             </div>
         );
     });
